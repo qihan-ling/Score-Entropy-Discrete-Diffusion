2ff906bc1bacd7cce793dd50002081b5d085284e
qihan96 <qihan96@stanford.edu>
2026-02-22 08:07:21 -0500

    update dataset files & relevant papers

---
+++ conversion_factor_analysis/get_gpt2_full.py
@@ -0,0 +1,79 @@
+# this script is copied from sapbenchmark github repo
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+import torch.nn.functional as F
+
+import argparse
+import csv
+import re
+import numpy as np
+
+from util import clean, align
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--cuda", action="store_true")
+parser.add_argument("--aligned", action="store_true")
+parser.add_argument("--input", type=str)
+parser.add_argument("--output", type=str)
+# TODO option for selecting subword merges to compute
+
+# how can we combine subwords/punctuation to get one surprisal per word?
+merge_fs = {"sum_":sum, "mean_": lambda x: sum(x)/len(x)}
+
+args = parser.parse_args()
+
+# Load models from HF transformers
+tokenizer = AutoTokenizer.from_pretrained("gpt2", add_prefix_space=True)
+model = AutoModelForCausalLM.from_pretrained("gpt2")
+
+if args.cuda:
+    device = torch.device("cuda")
+    model.to(device)
+
+
+in_f = open(args.input, "r")
+stims = csv.DictReader(in_f)
+
+out = []
+for stim_row in stims:
+    inputs = tokenizer("<|endoftext|> " + stim_row["Sentence"], return_tensors="pt")
+    ids = inputs["input_ids"]
+    tokens = tokenizer.tokenize(stim_row["Sentence"])
+
+    # run the model
+    outputs = model(**inputs, labels=ids)
+    logprobs = F.log_softmax(outputs.logits[0], 1)
+    
+    if args.aligned:
+        words = stim_row["Sentence"].split()
+        piecess, breaks = align(words, tokens)
+        # get surp for each word (avgd over pieces) and write it to a new row
+        for i, (word, pieces) in enumerate(zip(words, piecess)):
+            row = stim_row.copy() # new object, not a reference
+            row["token"] = ".".join(pieces)
+            row["word"] = word
+            row["word_pos"] = i
+            # correct for alignment difference due to initial EOS in the model input. see get_lstm.py for details
+            surps = [-logprobs[j][ids[0][j+1]].item() for j in range(breaks[i], breaks[i+1])]
+            surps_base2 = [surp/np.log(2.0) for surp in surps]
+            for merge_fn, merge_f in merge_fs.items():
+                row[merge_fn + "surprisal"] = merge_f(surps)
+                row[merge_fn + "surprisal_base2"] = merge_f(surps_base2)
+
+            out.append(row)
+
+    else:
+        # get surp for each wordpiece and write it to a new row
+        for i, (token, idx) in enumerate(zip(tokens, ids[0][1:])):
+            row = stim_row.copy() # new object, not a reference
+            row["token"] = token
+            row["word"] = clean(token)
+            row["word_pos"] = i
+            row["surprisal"] = -logprobs[i][idx].item()
+            out.append(row)
+
+with open(args.output, "w") as out_f:
+    writer = csv.DictWriter(out_f, fieldnames = out[0].keys())
+    writer.writeheader()
+    writer.writerows(out)
+
---
+++ conversion_factor_analysis/preprocess_csv.py
@@ -0,0 +1,37 @@
+# this script is copied from sapbenchmark github repo
+import pandas
+
+# Filenames to preprocess
+# TODO: Maybe automate gathering this, or put this in a shared config accessable by the scripts?
+main_name = "data/items_ClassicGP"
+other_names = ["data/items_Agreement", "data/items_AttachmentAmbiguity", "data/items_RelativeClause", "data/items_filler", "data/items_plaus"]
+
+def clean_sent(s):
+    """ Strip the sentences in the datafile of any weird escape characters so they match what participants saw """
+    if type(s) == str: return s.replace('\\', '')
+    return ""
+
+# Special processing for classic subset, since the ambig/unambig stimuli are in the same row
+main_df = pandas.read_csv(main_name + ".csv")
+main_df = main_df.melt(id_vars=["item", "condition", "disambPositionAmb", "disambPositionUnamb"], value_vars=["ambiguous", "unambiguous"], 
+             var_name="ambiguity", value_name="Sentence")
+main_df["disambPosition_0idx"] = [ambpos-1 if cond == "ambiguous" else unambpos-1 #0 indexing  
+                             for (ambpos, unambpos, cond) 
+                             in zip(main_df["disambPositionAmb"], main_df["disambPositionUnamb"], main_df["ambiguity"])]
+main_df["condition"] = ["{}_{}".format(cond.split("_")[0], "AMB" if amb == "ambiguous" else "UAMB")
+                        for (cond, amb) in zip(main_df["condition"], main_df["ambiguity"])]  
+
+main_df = main_df[main_df["Sentence"] != ""] 
+main_df["Sentence"] = main_df["Sentence"].apply(clean_sent)
+
+main_df.to_csv(main_name + ".pivot.csv")
+
+# For others, filter out empty rows and get a 0idx'd version of disambPosition if it exists for that dataset
+
+for name in other_names:
+    df = pandas.read_csv(name + ".csv")
+    df = df[df["Sentence"] != ""] 
+    df["Sentence"] = df["Sentence"].apply(clean_sent)
+    if "disambPosition" in df:
+        df["disambPosition_0idx"] = df["disambPosition"]
+    df.to_csv(name + ".pivot.csv")
